54c3b0b2184d701b1cff9a84c91ca869ede539e1
Michael Dariam Sandoval Lancheros <michael.sandoval@sinco.com.co>
2023-02-22 12:45:01 -0500

    primer comit

---
+++ src/Componentes/ComponentCamera.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+// import Stack from '@mui/material/Stack';
+// import Button from '@mui/material/Button';
+import { Camara } from "./Camara";
+
+export default function BotonCamara() {
+  return (
+    <Camara/>
+  );
+}
